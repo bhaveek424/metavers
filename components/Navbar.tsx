cdb07b42bf8128973025945bb7b1df0e170e5551
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-06-24 14:31:15 +0530

    Navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,31 @@
 'use client';
+import { motion } from 'framer-motion';
 
-const Navbar = () => <nav>navbar</nav>;
+import styles from '../styles';
+import { navVariants } from '@/utils/motion';
+import Image from 'next/image';
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}>
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <Image
+        src="/search.svg"
+        alt="Search"
+        width={24}
+        height={24}
+        className="object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+        METAVERSUS
+      </h2>
+      <Image width={24} height={24} src="/menu.svg" alt="menu" />
+    </div>
+  </motion.nav>
+);
 
 export default Navbar;
