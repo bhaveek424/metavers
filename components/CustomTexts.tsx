123e643eedd7dd51b363653d03e39ee2656923ec
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-06-24 18:01:30 +0530

    About section

--- components/CustomTexts.tsx
+++ components/CustomTexts.tsx
@@ -1,5 +1,18 @@
 'use client';
+import { motion } from 'framer-motion';
 
-export const TypingText = () => <p>Typing Text</p>;
+import { textContainer, textVariant2 } from '@/utils/motion';
+
+export const TypingText = ({ title, textStyles }: any) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
+    {Array.from(title).map((letter: any, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
+);
 
 export const TitleText = () => <h2>Title Text</h2>;
