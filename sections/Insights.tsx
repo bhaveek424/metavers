e916ee3f179170ed737ff7e02706982fec27888a
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-06-27 10:51:13 +0530

    Wrold section

--- sections/Insights.tsx
+++ sections/Insights.tsx
@@ -1,5 +1,10 @@
 'use client';
+import { motion } from 'framer-motion';
 
+import styles from '../styles';
+import { TypingText, TitleText, InsightCard } from '@/components';
+import { staggerContainer } from '@/utils/motion';
+import { insights } from '@/constants';
 const Insights = () => <section>Insights section</section>;
 
 export default Insights;
